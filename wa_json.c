c10262efb22760605550cedf0cf464603657211d
Brett Kuskie <fullaxx@gmail.com>
2022-12-14 03:55:56 -0700

    Update License

--- wa_json.c
+++ wa_json.c
@@ -1,3 +1,26 @@
+/*
+	A command-line utility to process JSON weather data
+	Copyright (C) 2022 Brett Kuskie <fullaxx@gmail.com>
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,9 +42,9 @@ static void process_forecast_location(cJSON *root)
 
 static void process_forecast_current(cJSON *root)
 {
-/*
 	cJSON *current_obj = cJSON_GetObjectItem(root, "current");
-*/
+	cJSON *temp_f_obj = cJSON_GetObjectItem(current_obj, "temp_f");
+	if(cJSON_IsNumber(temp_f_obj)) { printf("temp_f: %d\n", temp_f_obj->valueint); }
 }
 
 static void process_forecast_forecast(cJSON *root)
